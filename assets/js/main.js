f29202697a552f8e59ad2d4f7efd6058a8f8d9dc
didjee2 <dieter08@live.be>
2018-12-09 15:48:11 +0100

    Fixed the link for C# (replacing # with %23 for url)

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ function onSkillClick(event) {
     if (event.target.tagName.toLowerCase() !== 'li') {
         return;
     }
-    let text = event.target.innerText;
+    let text = event.target.innerText.replace('#', '%23');
 
     window.open('https://www.ecosia.org/search?q=' + text, '_blank');
 }
